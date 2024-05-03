99e55538c3b5b658f637bddfcc6fd1423a929c6a
Jeroen Peeters <contact@jeroenpeeters.be>
2024-05-04 01:48:17 +0200

    fix: apply the remaining classnames

--- packages/tailwind-plugin/src/utils/apply.util.ts
+++ packages/tailwind-plugin/src/utils/apply.util.ts
@@ -4,23 +4,28 @@ import { twMerge, type ClassNameValue } from "tailwind-merge";
 
 export function apply(...classes: Array<CSSRuleObject | ClassNameValue>): CSSRuleObject {
   const appliedObjectsArray: Array<CSSRuleObject> = [];
-  let appliedStringsArray = [];
+  let appliedStringsArray: Array<string> = [];
+
+  function attachStringsArray() {
+    if (appliedStringsArray.length > 0) {
+      appliedObjectsArray.push({
+        [`@apply ${twMerge(...appliedStringsArray)}`]: {},
+      });
+
+      appliedStringsArray = [];
+    }
+  }
 
   for (const stylesOrClass of classes) {
     if (typeof stylesOrClass === "string") {
       appliedStringsArray.push(stylesOrClass);
     } else {
-      if (appliedStringsArray.length > 0) {
-        appliedObjectsArray.push({
-          [`@apply ${twMerge(...appliedStringsArray)}`]: {},
-        });
-
-        appliedStringsArray = [];
-      }
-
+      attachStringsArray();
       appliedObjectsArray.push(stylesOrClass as CSSRuleObject);
     }
   }
+
+  attachStringsArray();
 
   return merge({}, ...appliedObjectsArray);
 }
