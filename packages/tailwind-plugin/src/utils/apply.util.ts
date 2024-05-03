a009e545da9c26d2f606ca33c44323eb0d1996ca
Jeroen Peeters <contact@jeroenpeeters.be>
2024-05-04 01:45:16 +0200

    fix: spread apply objects anywhere in args

--- packages/tailwind-plugin/src/utils/apply.util.ts
+++ packages/tailwind-plugin/src/utils/apply.util.ts
@@ -1,28 +1,26 @@
+import merge from "lodash.merge";
 import type { CSSRuleObject } from "tailwindcss/types/config";
 import { twMerge, type ClassNameValue } from "tailwind-merge";
 
-export function apply(
-  stylesOrClass: CSSRuleObject | ClassNameValue,
-  ...classes: ClassNameValue[]
-): CSSRuleObject {
-  const styles = typeof stylesOrClass === "string" ? {} : stylesOrClass;
-  const firstClass = typeof stylesOrClass === "string" ? stylesOrClass : "";
+export function apply(...classes: Array<CSSRuleObject | ClassNameValue>): CSSRuleObject {
+  const appliedObjectsArray: Array<CSSRuleObject> = [];
+  let appliedStringsArray = [];
 
-  return {
-    ...styles,
-    [`@apply ${twMerge(firstClass, ...(classes ?? []))}`]: {},
-  } as CSSRuleObject;
+  for (const stylesOrClass of classes) {
+    if (typeof stylesOrClass === "string") {
+      appliedStringsArray.push(stylesOrClass);
+    } else {
+      if (appliedStringsArray.length > 0) {
+        appliedObjectsArray.push({
+          [`@apply ${twMerge(...appliedStringsArray)}`]: {},
+        });
+
+        appliedStringsArray = [];
+      }
+
+      appliedObjectsArray.push(stylesOrClass as CSSRuleObject);
+    }
+  }
+
+  return merge({}, ...appliedObjectsArray);
 }
-
-apply.top = function (
-  stylesOrClass: CSSRuleObject | ClassNameValue,
-  ...classes: ClassNameValue[]
-): CSSRuleObject {
-  const styles = typeof stylesOrClass === "string" ? {} : stylesOrClass;
-  const firstClass = typeof stylesOrClass === "string" ? stylesOrClass : "";
-
-  return {
-    [`@apply ${twMerge(firstClass, ...(classes ?? []))}`]: {},
-    ...styles,
-  } as CSSRuleObject;
-};
