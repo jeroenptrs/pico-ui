ea34f53163278b44bd48b1f283275d804129c1f3
Jeroen Peeters <contact@jeroenpeeters.be>
2024-05-04 18:28:07 +0200

    feat: themes.azure

--- packages/tailwind-plugin/src/config/themes.config.ts
+++ packages/tailwind-plugin/src/config/themes.config.ts
@@ -1,7 +1,15 @@
 import { themesObject } from "@utils/themes.util";
 
 export const azure = {
-  primary: themesObject("azure-350", "azure-550"),
+  textSelectionColor: themesObject("azure-400/25", "azure-350/[.1875]"),
+  primary: themesObject("azure-550/100", "azure-350/100"),
+  primaryBackground: themesObject("azure-550/100", "azure-550/100"),
+  primaryUnderline: themesObject("azure-550/50", "azure-350/50"),
+  primaryHover: themesObject("azure-650/100", "azure-250/100"),
+  primaryHoverBackground: themesObject("azure-600/100", "azure-500/100"),
+  primaryFocus: themesObject("azure-400/50", "azure-350/[.375]"),
+  primaryInverse: themesObject("white", "white"),
+  switchThumbBoxShadow: themesObject("0 0 0 rgba(0, 0, 0, 0)", "0 0 0 rgba(0, 0, 0, 0)"),
 };
 
 export default {
